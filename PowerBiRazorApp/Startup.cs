6b3170fff044a958946c8c753cd0ee9039085820
BaakWu <wongyalan@gmail.com>
2018-09-16 22:16:20 -0500

    removed unecessary domain and text documents

--- PowerBiRazorApp/Startup.cs
+++ PowerBiRazorApp/Startup.cs
@@ -57,7 +57,6 @@ namespace PowerBiRazorApp
     public class AzureAdSettings
     {
         public string Instance { get; set; }
-        public string Domain { get; set; }
         public string TenantId { get; set; }
         public string ClientId { get; set; }
         public string CallbackPath { get; set; }
